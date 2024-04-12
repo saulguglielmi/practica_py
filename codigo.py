b6b13a11d47bc01775500a50d2c1efb0576840d9
saulguglielmi <saulguglielmi@gmail.com>
2024-04-11 19:46:23 -0600

    supongo que ya esta

--- codigo.py
+++ codigo.py
@@ -1,4 +1,4 @@
-from funciones import cuadrado, tabla
+from funciones import cuadrado, tabla , nosecomosellame
 
 numero = float(input("Escriba el largo o ancho de su cuadrado: "))
 cuadrado(numero)
@@ -6,5 +6,8 @@ cuadrado(numero)
 numtabla = int(input("Escriba el número para la tabla de multiplicar: "))
 tabla(numtabla)
 
+num1 = int(input("Escriba un numero para las operaciones: "))
+num2 = int(input("Escriba un segundo numero para las operaciones: "))
+nosecomosellame (num1,num2)
 
 
