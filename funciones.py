3c2a3c9b26da51b69328e32c6bb1fe5c1f8f3387
saulguglielmi <saulguglielmi@gmail.com>
2024-04-11 19:41:57 -0600

    añadido

--- funciones.py
+++ funciones.py
@@ -6,5 +6,10 @@ def tabla(numero):
     for contador in range(1,11):
         print(f"{numero} X {contador} = {numero*contador}")
 
-#se me olvido que mas iba XD
+def nosecomosellame(num1,num2):
+    print("Operaciones :D :")
+    print(f"{num1} + {num2} = {num1 + num2}")
+    print(f"{num1} - {num2} = {num1 - num2}")
+    print(f"{num1} x {num2} = {num1 * num2}")
+    print(f"{num1} / {num2} = {num1 / num2}")
 
